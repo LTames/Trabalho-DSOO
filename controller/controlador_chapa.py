55bf571a37f1c6f7452d929f920da895a08c65ee
Dem0lutio <luisfelipe@feyh.com.br>
2022-10-21 21:37:18 -0300

    alo

--- controller/controlador_chapa.py
+++ controller/controlador_chapa.py
@@ -1,9 +1,36 @@
 from abstracts.abstract_controlador import AbstractControlador
-
+from view.tela_chapa import TelaChapa
+from model.chapa import Chapa
 
 class ControladorChapa(AbstractControlador):
-    def __init__(self, controlador_urna):
-        pass
+    def __init__(self, controlador_urna) -> None:
+        self.__controlador_urna = controlador_urna
+        self.__tela_chapa = TelaChapa()
+        self.__chapas = []
 
-    def inicia_tela(self):
-        pass
+    @property
+    def controlador_urna(self):
+        return self.controlador_urna
+    
+    @property
+    def tela_chapa(self) -> TelaChapa:
+        return self.tela_chapa
+    
+    @property
+    def chapas(self) -> list:
+        return self.chapas
+    
+    def adiciona_chapa(self):
+        try:
+            dados_chapa = self.tela_chapa.get_dados_chapa()
+            for candidato in self.candidatos:
+                if dados_chapa["numero"] == candidato.numero:
+                    raise ValueError
+
+            self.chapas.append(Chapa(dados_chapa["num"],
+                                             dados_chapa["nome"],
+                                             dados_chapa["candidatos"]))
+                                             
+
+        except ValueError:
+            self.tela_chapa.alert(f"{'=' * 8} CHAPA JÁ CADASTRADA COM ESSE NÚMERO {'=' * 8}")
--- controller/controlador_eleitor.py
+++ controller/controlador_eleitor.py
@@ -1,9 +1,37 @@
 from abstracts.abstract_controlador import AbstractControlador
-
+from view.tela_eleitor import TelaEleitor
+from model.eleitor import Eleitor
 
 class ControladorEleitor(AbstractControlador):
-    def __init__(self, controlador_urna):
-        pass
+    def __init__(self, controlador_urna) -> None:
+        self.__controlador_urna = controlador_urna
+        self.__tela_eleitor = TelaEleitor()
+        self.__eleitores = []
 
-    def inicia_tela(self):
-        pass
+    @property
+    def controlador_urna(self):
+        return self.controlador_urna
+
+    @property
+    def tela_eleitor(self) -> TelaEleitor:
+        return self.tela_eleitor
+
+    @property
+    def eleitores(self) -> list:
+        return self.eleitores
+
+    def adiciona_eleitor(self):
+        try:
+            dados_eleitor = self.tela_eleitor.get_dados_eleitor()
+            for eleitor in self.eleitores:
+                if dados_eleitor["numero"] == eleitor.cpf:
+                    raise ValueError
+
+            self.eleitores.append(Eleitor(dados_eleitor["cpf"],
+                                             dados_eleitor["nome"],
+                                             dados_eleitor["email"],
+                                             dados_eleitor["endereco"],
+                                             dados_eleitor["tipo_eleitor"]))
+                                             
+        except ValueError:
+            self.tela_eleitor.alert(f"{'=' * 8} ELEITOR JÁ CADASTRADO COM ESSE CPF {'=' * 8}")
